9a699cc65fe38205eb1d51f3a11fc4e4e709842a
Lorenz Gorse <lorenz.gorse@gmail.com>
2013-12-06 23:52:09 +1100

    Use strict.

--- scripts/main.js
+++ scripts/main.js
@@ -48,6 +48,8 @@ require([
 	'passio/login',
 	'passio/pass-list'
 ], function (angular) {
+	'use strict';
+
 	var passio = angular.module('passio', ['ngRoute', 'passio.login', 'passio.passList']);
 
 	passio.config(['$routeProvider', function ($routeProvider) {
