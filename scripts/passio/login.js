5a937bd0e554581d2c059bb8185fea1fccf8774d
Lorenz Gorse <lorenz.gorse@gmail.com>
2014-12-08 17:17:06 +0100

    Remember username on successful login.

--- scripts/passio/login.js
+++ scripts/passio/login.js
@@ -8,17 +8,48 @@
 
 		var login = angular.module('passio.login', ['passio.factories']);
 
+		login.factory('UsernameHistoryService', function () {
+			var UsernameHistoryService = {};
+
+			UsernameHistoryService.localStorageSupported = function () {
+				return 'localStorage' in window && window.localStorage != null;
+			}
+
+			UsernameHistoryService.getMostRecent = function () {
+				if (!this.localStorageSupported()) {
+					return '';
+				}
+
+				var mostRecent = localStorage.getItem('UsernameHistoryService.mostRecent');
+				return mostRecent ? mostRecent : '';
+			}
+
+			UsernameHistoryService.setMostRecent = function (username) {
+				if (!this.localStorageSupported()) return;
+				localStorage.setItem('UsernameHistoryService.mostRecent', username);
+			}
+
+			return UsernameHistoryService;
+		});
+
 		login.controller('LoginController', [
 			'$scope',
 			'$location',
+			'UsernameHistoryService',
 			'PasswordServiceFactory',
-			function ($scope, $location, PasswordServiceFactory) {
+			function ($scope, $location, UsernameHistoryService, PasswordServiceFactory) {
+				$scope.user = {
+					name: UsernameHistoryService.getMostRecent(),
+					password: ''
+				};
+
 				$scope.doLogin = function () {
 					$scope.loginInProgress = true;
 
 					PasswordServiceFactory.create(
 						$scope.user.name, $scope.user.password
 					).then(function () {
+						UsernameHistoryService.setMostRecent($scope.user.name);
 						$location.path('/' + $scope.user.name).replace();
 						$scope.loginInProgress = false;
 					}, function () {
